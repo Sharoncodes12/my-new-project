8ff6af3f1b557994d91a56b04b213c7094e769a0
Sharoncodes12 <113323639+Sharoncodes12@users.noreply.github.com>
2022-10-06 20:12:24 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 function displayTemperature(response) {
-  console.log(apiUrl);
 
-  let cityElement = document.querySelector("#city");
-  let temperatureElement = document.querySelector("#temperature");
-  let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
-  let descriptionElement = document.querySelector("#description");
-
-  cityElement.innerHTML = response.data.name;
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  humidityElement.innerHTML = Math.round(response.data.main.humidity);
-  windElement.innerHTML = Math.round(response.data.wind.speed);
-  descriptionElement.innerHTML = response.data.weather[0].main;
+  console.log(response.data);
 
   let apiKey = "de3c7a97f3372cdd58943ef92e9f54b6";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
